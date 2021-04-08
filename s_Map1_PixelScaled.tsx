e46a70742eb31356943ada23186e260ac62a2ae4
pierrexuso <82162762+pierrexuso@users.noreply.github.com>
2021-04-08 18:38:50 +0200

    Delete s_Map1_PixelScaled.tsx

--- s_Map1_PixelScaled.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="s_Map1_PixelScaled" tilewidth="16" tileheight="16" tilecount="2304" columns="48">
- <image source="../workadventure-tutorial-master/s_Map1_PixelScaled.png" width="768" height="768"/>
-</tileset>
